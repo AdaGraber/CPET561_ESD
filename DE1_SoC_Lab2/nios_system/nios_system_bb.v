acb530eb251e5a8d04f46faef4cde56afbbc84fc
Ada Graber <ilg9251@rit.edu>
2026-09-15 15:54:52 -0400

    lab 2 update, add assembly code

--- DE1_SoC_Lab2/nios_system/nios_system_bb.v
+++ DE1_SoC_Lab2/nios_system/nios_system_bb.v
@@ -1,14 +1,14 @@
 
 module nios_system (
 	clk_clk,
-	reset_reset_n,
 	hex_export,
 	pushbuttons_export,
+	reset_reset_n,
 	switches_export);	
 
 	input		clk_clk;
-	input		reset_reset_n;
 	output	[6:0]	hex_export;
-	input	[3:0]	pushbuttons_export;
+	input	[2:0]	pushbuttons_export;
+	input		reset_reset_n;
 	input	[7:0]	switches_export;
 endmodule
